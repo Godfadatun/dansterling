bbf900769684c2ffe1307ba7fc24d035c1e9f537
Godfadatun <danyadegoke@gmail.com>
2020-07-19 02:27:07 -0400

    planets

--- src/components/Home/dPlanets.vue
+++ src/components/Home/dPlanets.vue
@@ -2,7 +2,7 @@
   <div :class="$q.screen.gt.sm ? 'q-px-xl': ''">
     <!-- {{myPlanets}} -->
     <subHeader :title="'Popular Planets'"/>
-    <q-carousel autoplay v-model="slide" transition-prev="slide-righ" transition-next="slide-left" swipeable animated control-color="grey-9" navigation padding  class="text-white q-my-md">
+    <q-carousel infinite autoplay v-model="slide" transition-prev="slide-righ" transition-next="slide-left" swipeable animated control-color="grey-9" navigation padding  class="text-white q-my-md">
       <q-carousel-slide v-for="(val, i) in myPlanets" :key="'A'+i" :name="'A'+i" class="row no-wrap flex-center" >
         <div v-for="(item, index) in val" :key="index" class="q-pa-sm">
           <planets class="col-md-4 col-sm-6 col-xs-12"  :info="item" :index='index' :url="url"/>
